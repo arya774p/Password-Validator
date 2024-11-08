48814791dc62721edb173d248b44ec708cfef7ad
Prerna Arya <arya774p@gmail.com>
2024-11-08 17:22:32 +0530

    Refactored the code.

--- main.py
+++ main.py
@@ -16,49 +16,43 @@ while True:
     
     # Password should not contain the word ‘password’ (case-insensitive)
     not_have_password = 'password' not in password.lower()
-    if not not_have_password :
+    if not not_have_password:
         print("password should not contain word password")
 
 
     # Password should contain at least one uppercase letter
     contains_upper = False
+    contains_lower = False
+    has_digit = False
+    has_special = False
+    special_chars = """!@#$%^&*()_+=}{][|\:;"'<>,.?/"""
     for char in password:
         if char.isupper():
             contains_upper = True
-            break
 
+        if ord(char) in range(97,123):  # 97 <= ord(char) <= 122
+            contains_lower = True
+            
+        if char.isdigit():
+            has_digit = True
+                
+        if char in special_chars:
+            has_special = True       
+    
+
+        if contains_upper and contains_lower and has_digit and special_chars:
+            break
+    
     if contains_upper==False:   # if not contains_upper
         print("Password should contain aleast one uppercase letter")
 
-    # Password should contain at least one lowercase letter - 97-122
-    contains_lower = False
-    for char in password:
-        if ord(char) in range(97,123):  # 97 <= ord(char) <= 122
-            contains_lower = True
-            break
-
     if not contains_lower:
         print("Pass should contain atleast one lowercase char")
-
-    # Password should contain at least one digit
-    has_digit = False
-    for char in password:
-        if char.isdigit():
-            if int(char) in range(0,10):
-                has_digit = True
-                break
-    if has_digit == False :
+        
+    if has_digit == False:
         print("Pass should contaon atleast one digit")
-
-    # Password should contain at least one special character (!, @, #, $, %, ^, &, *, (, ), _, +, =, {, }, [, ], |, \, :, ;, ", ', <, >, ,, ., ?, /)
-
-    special_chars = """!@#$%^&*()_+=}{][|\:;"'<>,.?/"""
-    has_special = False
-    for char in password:
-        if char in special_chars :
-            has_special = True
-            break
-    if not has_special :
+        
+    if not has_special:
         print("Password should contain atleast one special character")
 
 
