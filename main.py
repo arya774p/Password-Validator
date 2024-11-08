f2484fe8d8c585637947a1a3eac7abed85b94b05
Prerna Arya <arya774p@gmail.com>
2024-11-08 16:22:31 +0530

    Made the code dynamic using While loop

--- main.py
+++ main.py
@@ -1,58 +1,68 @@
+while True:
+    password=input("Enter a password : ")
+
+    atleast_8 = len(password) >= 8
+    if not atleast_8:
+        print("Password should be atleast 8 characters long")
+    
+    atmost_20 = len(password) <= 20
+    if not atmost_20: 
+        print("Password should be atmost 20 characters long")
+
+    # Password should not contain any whitespace
+    no_whitespace = ' ' not in password    # gives True if no whitespace else False
+    if not no_whitespace:   # gives True if there is whitespace else False
+        print("Password should not contain any whitespace")
+    
+    # Password should not contain the word ‘password’ (case-insensitive)
+    not_have_password = 'password' not in password.lower()
+    if not not_have_password :
+        print("password should not contain word password")
 
 
-password=input("Enter a password : ")
-
-if len(password) >= 8:
-    print("Atleast 8 digit validation checked")
-
-if len(password) <= 20:
-    print("Atmost 20 digit validation checked")
-
-# Password should not contain any whitespace
-if ' ' not in password:
-    print("no whitespace validation checked")
-
-# Password should not contain the word ‘password’ (case-insensitive)
-if 'password' not in password.lower():
-    print("password validation checked")
-
-# Password should contain at least one uppercase letter
-contains_upper = False
-for char in password:
-    if char.isupper():
-        contains_upper = True
-        break
-
-if contains_upper:
-    print("uppercase validation checked")
-
-# Password should contain at least one lowercase letter - 97-122
-contains_lower = False
-for char in password:
-    if ord(char) in range(97,123):  # 97 <= ord(char) <= 122
-        contains_lower = True
-        break
-
-if contains_lower:
-    print("lowercase validation checked")
-
-# Password should contain at least one digit
-has_digit = False
-for char in password:
-    if char.isdigit():
-        if int(char) in range(0,10):
-            has_digit = True
+    # Password should contain at least one uppercase letter
+    contains_upper = False
+    for char in password:
+        if char.isupper():
+            contains_upper = True
             break
-if has_digit:
-    print("atleast one digit validation checked")
 
-# Password should contain at least one special character (!, @, #, $, %, ^, &, *, (, ), _, +, =, {, }, [, ], |, \, :, ;, ", ', <, >, ,, ., ?, /)
+    if contains_upper==False:   # if not contains_upper
+        print("Password should contain aleast one uppercase letter")
 
-special_chars = """!@#$%^&*()_+=}{][|\:;"'<>,.?/"""
-has_special = False
-for char in password:
-    if char in special_chars :
-        has_special = True
+    # Password should contain at least one lowercase letter - 97-122
+    contains_lower = False
+    for char in password:
+        if ord(char) in range(97,123):  # 97 <= ord(char) <= 122
+            contains_lower = True
+            break
+
+    if not contains_lower:
+        print("Pass should contain atleast one lowercase char")
+
+    # Password should contain at least one digit
+    has_digit = False
+    for char in password:
+        if char.isdigit():
+            if int(char) in range(0,10):
+                has_digit = True
+                break
+    if has_digit == False :
+        print("Pass should contaon atleast one digit")
+
+    # Password should contain at least one special character (!, @, #, $, %, ^, &, *, (, ), _, +, =, {, }, [, ], |, \, :, ;, ", ', <, >, ,, ., ?, /)
+
+    special_chars = """!@#$%^&*()_+=}{][|\:;"'<>,.?/"""
+    has_special = False
+    for char in password:
+        if char in special_chars :
+            has_special = True
+            break
+    if not has_special :
+        print("Password should contain atleast one special character")
+
+
+    if atleast_8 and atmost_20 and no_whitespace and not_have_password and contains_upper and contains_lower and has_digit and has_special:
+        print("Password is valid")
         break
-if has_special :
-    print("contains special characters")
+
